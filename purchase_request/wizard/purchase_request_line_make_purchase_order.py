e86e60bd57eb1d401bacf37435c83b1ae063b2ad
Thomas Binsfeld <thomas.binsfeld@acsone.eu>
2021-05-18 14:29:11 +0200

    [REF] purchase_request: deactivate product qty overwrite at PO creation

--- purchase_request/wizard/purchase_request_line_make_purchase_order.py
+++ purchase_request/wizard/purchase_request_line_make_purchase_order.py
@@ -236,7 +236,6 @@ class PurchaseRequestLineMakePurchaseOrder(models.TransientModel):
         res = []
         purchase_obj = self.env["purchase.order"]
         po_line_obj = self.env["purchase.order.line"]
-        pr_line_obj = self.env["purchase.request.line"]
         purchase = False
 
         for item in self.item_ids:
@@ -259,7 +258,6 @@ class PurchaseRequestLineMakePurchaseOrder(models.TransientModel):
             # po line
             domain = self._get_order_line_search_domain(purchase, item)
             available_po_lines = po_line_obj.search(domain)
-            new_pr_line = True
             # If Unit of Measure is not set, update from wizard.
             if not line.product_uom_id:
                 line.product_uom_id = item.product_uom_id
@@ -267,7 +265,6 @@ class PurchaseRequestLineMakePurchaseOrder(models.TransientModel):
             alloc_uom = line.product_uom_id
             wizard_uom = item.product_uom_id
             if available_po_lines and not item.keep_description:
-                new_pr_line = False
                 po_line = available_po_lines[0]
                 po_line.purchase_request_lines = [(4, line.id)]
                 po_line.move_dest_ids |= line.move_dest_ids
@@ -292,18 +289,6 @@ class PurchaseRequestLineMakePurchaseOrder(models.TransientModel):
                 )
                 all_qty = min(po_line_product_uom_qty, wizard_product_uom_qty)
                 self.create_allocation(po_line, line, all_qty, alloc_uom)
-            # TODO: Check propagate_uom compatibility:
-            new_qty = pr_line_obj._calc_new_qty(
-                line, po_line=po_line, new_pr_line=new_pr_line
-            )
-            po_line.product_qty = new_qty
-            po_line._onchange_quantity()
-            # The onchange quantity is altering the scheduled date of the PO
-            # lines. We do not want that:
-            date_required = item.line_id.date_required
-            po_line.date_planned = datetime(
-                date_required.year, date_required.month, date_required.day
-            )
             res.append(purchase.id)
 
         return {
